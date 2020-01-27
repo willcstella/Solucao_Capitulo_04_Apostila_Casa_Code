26171731f76edfe5583b2f6809618e46c3ff94b6
william stella <willcstella@gmail.com>
2020-01-27 16:05:46 -0300

    Adicionado um btn gravar

--- ViewProject/FormNotaEntrada.Designer.cs
+++ ViewProject/FormNotaEntrada.Designer.cs
@@ -52,6 +52,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btn_Novo_Prod_Comprados = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -328,11 +329,22 @@
             this.btn_Novo_Prod_Comprados.Text = "Novo";
             this.btn_Novo_Prod_Comprados.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gravar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormNotaEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_Novo_Prod_Comprados);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -376,5 +388,6 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button btn_Novo_Prod_Comprados;
+        private System.Windows.Forms.Button button1;
     }
 }
