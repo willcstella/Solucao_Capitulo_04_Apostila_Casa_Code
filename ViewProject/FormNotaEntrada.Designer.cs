422ac288798060abc71ba9028db0f3a7de57187f
william stella <willcstella@gmail.com>
2020-01-28 02:20:44 -0300

    Aterado o tamanho do botao novo e gravar da view produtos comprados

--- ViewProject/FormNotaEntrada.Designer.cs
+++ ViewProject/FormNotaEntrada.Designer.cs
@@ -69,7 +69,7 @@
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.2807F));
@@ -93,7 +93,7 @@
             this.tableLayoutPanel2.Controls.Add(this.dateTimePicker1, 1, 3);
             this.tableLayoutPanel2.Controls.Add(this.dateTimePicker2, 1, 4);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 40);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 5;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
@@ -213,7 +213,7 @@
             this.tableLayoutPanel3.Controls.Add(this.textBox4, 1, 2);
             this.tableLayoutPanel3.Controls.Add(this.textBox5, 1, 3);
             this.tableLayoutPanel3.Location = new System.Drawing.Point(545, 40);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 4;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
@@ -322,9 +322,9 @@
             // btn_Novo_Prod_Comprados
             // 
             this.btn_Novo_Prod_Comprados.Location = new System.Drawing.Point(553, 325);
-            this.btn_Novo_Prod_Comprados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Novo_Prod_Comprados.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Novo_Prod_Comprados.Name = "btn_Novo_Prod_Comprados";
-            this.btn_Novo_Prod_Comprados.Size = new System.Drawing.Size(100, 28);
+            this.btn_Novo_Prod_Comprados.Size = new System.Drawing.Size(72, 28);
             this.btn_Novo_Prod_Comprados.TabIndex = 1;
             this.btn_Novo_Prod_Comprados.Text = "Novo";
             this.btn_Novo_Prod_Comprados.UseVisualStyleBackColor = true;
@@ -334,7 +334,7 @@
             this.button1.Location = new System.Drawing.Point(661, 325);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(72, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = "Gravar";
             this.button1.UseVisualStyleBackColor = true;
@@ -348,7 +348,7 @@
             this.Controls.Add(this.btn_Novo_Prod_Comprados);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormNotaEntrada";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registro de notas de entrada";
